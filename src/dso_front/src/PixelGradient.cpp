9bcd712dba0d31d39c04c6ddc72a57cf4ca79c75
Elkulas <elkulasjiang@gmail.com>
2021-11-23 14:50:29 +0800

    refine 16bit test

--- src/dso_front/src/PixelGradient.cpp
+++ src/dso_front/src/PixelGradient.cpp
@@ -30,14 +30,14 @@ void PixelGradient::computeGradents(const Mat img)
     hG[level] = hG[level - 1] / 2;
   }
 
-  // 所有的像素数量
-  int tolPixel = img.cols * img.rows;
+  // // 所有的像素数量
+  // int tolPixel = img.cols * img.rows;
 
-  // 指向uchar的指针
-  unsigned char *color = new unsigned char[tolPixel];
+  // // 指向uchar的指针
+  // unsigned char *color = new unsigned char[tolPixel];
 
-  // 将原来在img中的数据拷贝到color分配的内存中
-  memcpy(color, img.data, img.rows*img.cols);
+  // // 将原来在img中的数据拷贝到color分配的内存中
+  // memcpy(color, img.data, img.rows*img.cols);
 
 
   // 为每一层图像梯度分配存储空间
@@ -55,9 +55,31 @@ void PixelGradient::computeGradents(const Mat img)
   dI = dIp[0]; 
 
   // 将img中的像素信息存到dI中
-  for (int i = 0; i < w * h; i++){
-    dI[i][0] = (float)color[i];
+  // for (int i = 0; i < w * h; i++){
+  //   dI[i][0] = (float)color[i];
+  // }
+
+
+  if(img.type() == 0) {
+    for (int j = 0; j < img.rows; ++j) {
+      for (int i = 0; i < img.cols; ++i) {
+        dI[i + j * img.cols][0] = (img.at<uchar>(j, i));
+      }
+    }
   }
+  else if(img.type() == 2) {
+    std::cout << "16bit!" << std::endl;
+    for (int j = 0; j < img.rows; ++j) {
+      for (int i = 0; i < img.cols; ++i) {
+        dI[i + j * img.cols][0] = (img.at<ushort>(j, i));
+      }
+    }
+  }
+  else {
+    std::cout << "Not Proper type!" << std::endl;
+  }
+
+
 
   // 遍历金字塔
   for (int lvl = 0; lvl < pyrLevelsUsed; lvl++) {
@@ -117,136 +139,6 @@ void PixelGradient::computeGradents(const Mat img)
 
 }
 
-
-void PixelGradient::computeGradentsFor16(const Mat img)
-{
-  // 初始的长宽
-  int w = img.cols;
-  int h = img.rows;
-
-
-
-  // 0层情况
-  // wG和hG是int的数组
-  wG[0] = w;
-  hG[0] = h;
-  // 构建金字塔
-  // 四层,在构造函数中定义
-  // 将金字塔各个层的长宽存下来
-  for (int level = 1; level < pyrLevelsUsed; ++level) {
-    wG[level] = wG[level - 1] / 2;
-    hG[level] = hG[level - 1] / 2;
-  }
-
-  // 所有的像素数量
-  int tolPixel = img.cols * img.rows;
-
-  // 指向uchar的指针
-  ushort *color = new ushort[tolPixel];
-
-  // 将原来在img中的数据拷贝到color分配的内存中
-  // 对于16位这一步有问题
-  // memcpy(color, (unsigned short*)img.data, img.rows*img.cols);
-  for (int j = 0; j < img.rows; ++j) {
-    for (int i = 0; i < img.cols; ++i) {
-      color[i + j * img.cols] = (img.at<ushort>(j, i));
-    }
-  }
-
-  // 为每一层图像梯度分配存储空间
-  // 用dIp来存储各个层的梯度情况,sq的存储梯度的平方
-  for (int i = 0; i < pyrLevelsUsed; i++) {
-    dIp[i] = new Eigen::Vector3f[wG[i] * hG[i]];
-   
-    absSquaredGrad[i] = new float[wG[i] * hG[i]];
-  }
-  
-
-  // dI指针指向了第0层的第一个元素
-  // 原来他们指向同一个地方,图像导数
-  dI = dIp[0]; 
-
-  float vmax = 0;
-  float vmin = 65556;
-
-  // 将img中的像素信息存到dI中
-  for (int i = 0; i < w * h; i++){
-    dI[i][0] = (float)color[i];
-      // TODO:
-    if((float)color[i] > vmax)
-      vmax = (float)color[i];
-    if((float)color[i] < vmin)
-      vmin = (float)color[i];
-  }
-  std::cout << vmax << '\t' << vmin << std::endl;
-
-  std::cout << img.type() << std::endl;
-
-  // 遍历金字塔
-  for (int lvl = 0; lvl < pyrLevelsUsed; lvl++) {
-    // 该层图像大小
-    int wl = wG[lvl], hl = hG[lvl]; 
-    // 指向当前层的像素
-    Eigen::Vector3f *dI_l = dIp[lvl];
-
-    // 对于平方的一个指针
-    float *dabs_l = absSquaredGrad[lvl];
-
-    // 如果不是第一层
-    if (lvl > 0) {
-      // 上一层
-      int lvlm1 = lvl - 1;
-      int wlm1 = wG[lvlm1]; // 列数
-      Eigen::Vector3f *dI_lm = dIp[lvlm1];
-
-
-      // 像素4合1, 生成金字塔
-      for (int y = 0; y < hl; y++)
-        for (int x = 0; x < wl; x++) {
-          dI_l[x + y * wl][0] = 0.25f * (dI_lm[2 * x + 2 * y * wlm1][0] +
-              dI_lm[2 * x + 1 + 2 * y * wlm1][0] +
-              dI_lm[2 * x + 2 * y * wlm1 + wlm1][0] +
-              dI_lm[2 * x + 1 + 2 * y * wlm1 + wlm1][0]);
-        }
-    }
-
-    // 平均gradients
-    float mean_grident = 0.0f;
-    // 第二行开始,倒数第二行结束
-    for (int idx = wl; idx < wl * (hl - 1); idx++)
-    {
-      float dx = 0.5f * (dI_l[idx + 1][0] - dI_l[idx - 1][0]);
-      float dy = 0.5f * (dI_l[idx + wl][0] - dI_l[idx - wl][0]);
-
-      if (!std::isfinite(dx)) dx = 0;
-      if (!std::isfinite(dy)) dy = 0;
-
-      // 0:原始像素值 1:x方向梯度值 2:y方向梯度值
-      dI_l[idx][1] = dx;
-      dI_l[idx][2] = dy;
-
-      dabs_l[idx] = dx * dx + dy * dy; // 梯度平方
-      mean_grident += sqrtf(dabs_l[idx]);
-    }
-    std::cout << "mean grident of level" << lvl << "=" << mean_grident / (wl * hl) 
-    << "\tsize = " << wl << " " << hl 
-    << std::endl;
-
-  } // 遍历完毕各个层
-
-  cv::Mat out(img.size(), CV_16UC1);
-  toCvMat16(absSquaredGrad[0], out);
-  cv::Mat out2(out.size(), CV_8UC1);
-  for (int j = 0; j < out2.rows; ++j) {
-    for (int i = 0; i < out2.cols; ++i) {
-      (out2.at<uchar>(j, i)) = (uchar)((float)(out.at<ushort>(j,i)/(vmax - vmin))*255.0); 
-    }
-  }
-  cv::imshow("hi", out2);
-  cv::waitKey();
-
-}
-
 void PixelGradient::computeGradentsWithMask(const Mat img, const Mat mask)
 {
 
@@ -275,16 +167,16 @@ void PixelGradient::computeGradentsWithMask(const Mat img, const Mat mask)
     hG[level] = hG[level - 1] / 2;
   }
 
-  // 所有的像素数量
-  int tolPixel = img.cols * img.rows;
+  // // 所有的像素数量
+  // int tolPixel = img.cols * img.rows;
 
-  // 指向uchar的指针
-  unsigned char *color = new unsigned char[tolPixel];
+  // // 指向uchar的指针
+  // unsigned char *color = new unsigned char[tolPixel];
   // 存放mask data指针
   unsigned char *dmask[PYR_LEVELS];
 
-  // 将原来在img中的数据拷贝到color分配的内存中
-  memcpy(color, img.data, img.rows*img.cols);
+  // // 将原来在img中的数据拷贝到color分配的内存中
+  // memcpy(color, img.data, img.rows*img.cols);
   // memcpy(dmask, mask.data, mask.rows*mask.cols);
 
 
@@ -307,9 +199,28 @@ void PixelGradient::computeGradentsWithMask(const Mat img, const Mat mask)
   // dI也就表示了第0层的情况
   dI = dIp[0]; 
 
-  // 将img中的像素信息存到dI中
-  for (int i = 0; i < w * h; i++)
-    dI[i][0] = (float)color[i];
+  // // 将img中的像素信息存到dI中
+  // for (int i = 0; i < w * h; i++)
+  //   dI[i][0] = (float)color[i];
+
+  if(img.type() == 0) {
+    for (int j = 0; j < img.rows; ++j) {
+      for (int i = 0; i < img.cols; ++i) {
+        dI[i + j * img.cols][0] = (img.at<uchar>(j, i));
+      }
+    }
+  }
+  else if(img.type() == 2) {
+    for (int j = 0; j < img.rows; ++j) {
+      for (int i = 0; i < img.cols; ++i) {
+        dI[i + j * img.cols][0] = (img.at<ushort>(j, i));
+      }
+    }
+  }
+  else {
+    std::cout << "Not Proper type!" << std::endl;
+  }
+
 
   // 遍历金字塔
   for (int lvl = 0; lvl < pyrLevelsUsed; lvl++) {
--- test/test_dso_front.cpp
+++ test/test_dso_front.cpp
@@ -24,72 +24,42 @@ int main(int argc, char* argv[]){
 
   Mat img0 = imread(FLAGS_dir, -1);
 
-  if(img0.type() == 0) {
-    // detect points
-    PixelGradient *pixelGradent_ = new PixelGradient;
-    pixelGradent_->computeGradents(img0);
 
-    PixelSelector::Ptr ps(new PixelSelector(img0.cols, img0.rows, 150, 5));
+  // detect points
+  PixelGradient *pixelGradent_ = new PixelGradient;
+  pixelGradent_->computeGradents(img0);
 
-    float *statusMap = new float[img0.cols * img0.cols];
+  PixelSelector::Ptr ps(new PixelSelector(img0.cols, img0.rows, 150, 5));
 
-    // detect
-    int num = ps->makeMaps(pixelGradent_, statusMap, 1, false, 2);
-    // detect ends
-    std::cout << "Get Point number'\t'" << num << std::endl;
-    cv::waitKey();
+  float *statusMap = new float[img0.cols * img0.cols];
 
-    cv::Mat output;
-    img0.copyTo(output);
-    cv::cvtColor(output, output, cv::COLOR_GRAY2RGB);
+  // detect
+  int num = ps->makeMaps(pixelGradent_, statusMap, 1, false, 2);
+  // detect ends
+  std::cout << "Get Point number'\t'" << num << std::endl;
 
-    // traverse points
-    for (int y = patternPadding + 1; y < img0.rows - patternPadding - 2; y++)
-        for (int x = patternPadding + 1; x < img0.cols - patternPadding - 2; x++) {
-          if(statusMap[x + y * img0.cols] != 0){
-            cv::circle(output, cv::Point(x, y), 3, cv::Scalar(0, 255, 0), 1);
-          }
-    }
+  // OUt put
+  cv::Mat output;
+  cv::normalize(img0, output, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+  cv::cvtColor(output, output, cv::COLOR_GRAY2RGB);
 
-    cv::imshow("output", output);
-
-    cv::waitKey();
+  // traverse points
+  for (int y = patternPadding + 1; y < img0.rows - patternPadding - 2; y++)
+      for (int x = patternPadding + 1; x < img0.cols - patternPadding - 2; x++) {
+        if(statusMap[x + y * img0.cols] != 0){
+          cv::circle(output, cv::Point(x, y), 3, cv::Scalar(0, 255, 0), 1);
+        }
   }
-
-  else {
-    std::cout << "Not 8bit!" << std::endl;
-    PixelGradient *pixelGradent_ = new PixelGradient;
-    pixelGradent_->computeGradentsFor16(img0);
-
-    PixelSelector::Ptr ps(new PixelSelector(img0.cols, img0.rows, 150, 5));
-
-    float *statusMap = new float[img0.cols * img0.cols];
-
-    // detect
-    int num = ps->makeMaps(pixelGradent_, statusMap, 1, false, 2);
-    // detect ends
-    std::cout << "Get Point number'\t'" << num << std::endl;
-    cv::waitKey();
-
-    cv::Mat output(img0.size(), CV_8UC1);
-
-    cv::normalize(img0, output, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+  
+  cv::imshow("output", output);
+  cv::waitKey();
 
 
-    cv::cvtColor(output, output, cv::COLOR_GRAY2RGB);
+  cv::Mat gradents = cv::Mat(img0.rows, img0.cols, CV_8UC1);
+  toCvMat(pixelGradent_->absSquaredGrad[0], gradents);
+  cv::imshow("graedient", gradents);
 
-    // traverse points
-    for (int y = patternPadding + 1; y < img0.rows - patternPadding - 2; y++)
-        for (int x = patternPadding + 1; x < img0.cols - patternPadding - 2; x++) {
-          if(statusMap[x + y * img0.cols] != 0){
-            cv::circle(output, cv::Point(x, y), 3, cv::Scalar(0, 255, 0), 1);
-          }
-    }
-
-    cv::imshow("output", output);
-
-    cv::waitKey();
-  }
+  cv::waitKey();
 
   
 
